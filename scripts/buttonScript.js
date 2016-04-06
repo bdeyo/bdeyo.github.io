16640def2561603788d0c36f66ad18be0a3cb123
Ben DeYoung <deyo0039@gmail.com>
2016-04-06 16:20:03 -0500

    getting button to change line multiple times

--- scripts/buttonScript.js
+++ scripts/buttonScript.js
@@ -1,3 +1,10 @@
 function myFunc(){
-			document.getElementById("test").innerHTML = "Word changed";
+			var val = 0;
+			if (val == 0) {
+				document.getElementById("test").innerHTML = "Word changed";
+				val++;
+			} else {
+				document.getElementById("test").innerHTML = "Word changed again!";
+				val--;
+			}
 		}
